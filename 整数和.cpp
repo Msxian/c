e70edda681518af7f1debf1fd291fd8a576564f1
Msxian <43413769+Msxian@users.noreply.github.com>
2018-09-24 21:40:59 +0800

    Add files via upload

---
+++ 整数和.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x, y, z;
+		cout << "输入两个整数：";
+	    cin >> x >> y;
+	    z=x+y;
+	    cout << "两数的和是：" << z;
+	system("pause");
+    return 0;
+}
